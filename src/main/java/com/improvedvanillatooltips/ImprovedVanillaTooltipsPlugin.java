c36a7dbf745b32b1160f62a2ea21ee3e7790215b
Zachary Jacobson <zach.h.jacobson@gmail.com>
2026-05-22 18:44:22 -0500

    Implement base functionality
    All documented tooltips appear immediately.  Prayer tooltips are modified to be textless, recolored, resized, and repositioned.

--- src/main/java/com/improvedvanillatooltips/ImprovedVanillaTooltipsPlugin.java
+++ src/main/java/com/improvedvanillatooltips/ImprovedVanillaTooltipsPlugin.java
@@ -4,6 +4,7 @@ import com.google.inject.Provides;
 import javax.inject.Inject;
 import lombok.extern.slf4j.Slf4j;
 import net.runelite.api.Client;
+import net.runelite.api.MenuEntry;
 import net.runelite.api.events.ScriptPostFired;
 import net.runelite.api.gameval.InterfaceID;
 import net.runelite.api.gameval.VarClientID;
@@ -13,22 +14,28 @@ import net.runelite.client.eventbus.Subscribe;
 import net.runelite.client.plugins.Plugin;
 import net.runelite.client.plugins.PluginDescriptor;
 
+import java.util.Set;
+
 @Slf4j
 @PluginDescriptor(
 	name = "Improved Vanilla Tooltips"
 )
 public class ImprovedVanillaTooltipsPlugin extends Plugin
 {
-	private static final int STYLE_ID = 526;			//also attack bar, audio icons, zoom, controls, emotes, map, prayer, emotes, brightness, zoom
-	private static final int RETALIATE_ID = 38;
-	private static final int STATS_ID = 992;
-	private static final int WORLDSWITCHER_ID = 7274;	//works in small and large menus
-	private static final int SKULL_PREVENTION_ID = 5524;
-	private static final int SETTINGS_OPEN_ID = 3904;
-	private static final int MASTER_ZOOM_ID = 313;
-	private static final int MUSIC_ZOOM_ID = 2257;
-	private static final int SOUND_ZOOM_ID = 3927;
-	private static final int AREASOUNDS_ZOOM_ID = 3928;
+	private static final Set<Integer> SCRIPT_IDS = Set.of(
+			526,	//attack style, attack bar, audio icons, zoom, controls, emotes, map, prayer, emotes, brightness, zoom
+			38,		//retaliate
+			992,	//stats
+			7274,	//world switcher, both small and large
+			5524,	//skull prevention
+			3904,	//settings open
+			313,	//master zoom
+			2257,	//music zoom
+			3927,	//sound zoom
+			3928	//area sounds zoom
+	);
+
+	private static final int TOOLTIP_ID = 526;
 
 	@Inject
 	private Client client;
@@ -39,16 +46,49 @@ public class ImprovedVanillaTooltipsPlugin extends Plugin
 	@Subscribe
 	public void onScriptPostFired(ScriptPostFired script)
 	{
-		if(script.getScriptId() == STYLE_ID)
+		int scriptID = script.getScriptId();
+		//show tooltips immediately
+		if(SCRIPT_IDS.contains(scriptID)) showTooltip();
+		//replace text in prayer tooltips
+		if(scriptID == TOOLTIP_ID) replacePrayerTooltip();
+	}
+
+	private void showTooltip()
+	{
+		int old_time = client.getVarcIntValue(VarClientID.TOOLTIP_TIME);
+		int new_time = old_time + 1000;
+		client.setVarcIntValue(VarClientID.TOOLTIP_TIME, new_time);
+	}
+
+	private void replacePrayerTooltip()
+	{
+		//get tooltip, tooltip components, the prayer being moused over, and tooltip bottom for later
+		Widget tooltip = client.getWidget(InterfaceID.Prayerbook.TOOLTIP);
+		assert tooltip != null;
+		Widget[] components = tooltip.getChildren();
+		if(components == null || components.length != 3) return;
+		Widget background = tooltip.getChild(0);
+		Widget border = tooltip.getChild(1);
+		Widget text = tooltip.getChild(2);
+		if(background == null || border == null || text == null) return;
+		MenuEntry[] menu_entries = client.getMenu().getMenuEntries();
+		Widget prayer = menu_entries[menu_entries.length - 1].getWidget();
+		assert prayer != null;
+		int tooltip_bottom = tooltip.getHeight() + tooltip.getOriginalY();
+
+		int custom_x = 100;
+		int custom_y = 50;
+		//modify the tooltip to be a solid color, textless, and relocated based on the prayer's position
+		background.setTextColor(0xFF0000);
+		text.setText("");
+		tooltip.setSize(custom_x, custom_y);
+		if(tooltip.getOriginalY() < prayer.getOriginalY())
 		{
-			Widget tooltip = client.getWidget(InterfaceID.Prayerbook.TOOLTIP);
-			if(tooltip != null)
-			{
-				int old_time = client.getVarcIntValue(VarClientID.TOOLTIP_TIME);
-				int new_time = old_time + 1000;
-				client.setVarcIntValue(VarClientID.TOOLTIP_TIME, new_time);
-			}
+			tooltip.setOriginalY(tooltip_bottom - custom_y);
 		}
+        tooltip.setOriginalX(prayer.getOriginalX() + (prayer.getWidth() / 2) - (custom_x / 2));
+		tooltip.revalidate();
+		border.revalidate();
 	}
 
 	@Provides
